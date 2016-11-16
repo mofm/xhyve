efb46fba3d71f2883626f8b92335f9b9768a1885
mofm <emre.eryilmaz@piesso.com>
2016-11-16 23:46:35 +0300

    adding gentoo install script

---
+++ gentoo/gentoo_install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+KERNEL="./gentoo"
+INITRD="./gentoo.igz"
+CMDLINE="root=/dev/ram0 init=/linuxrc  dokeymap looptype=squashfs loop=/image.squashfs cdroot earlyprintk=serial console=ttyS0"
+
+MEM="-m 1G"
+NET="-s 2:0,virtio-net"
+IMG_CD="-s 3,ahci-cd,/Volumes/DiskX/ISO/install-amd64-minimal-20160804.iso"
+IMG_HDD="-s 4,virtio-blk,gentoo.img"
+PCI_DEV="-s 0:0,hostbridge -s 31,lpc"
+LPC_DEV="-l com1,stdio"
+ACPI="-A"
+
+sudo xhyve $ACPI $MEM $PCI_DEV $LPC_DEV $NET $IMG_CD $IMG_HDD -f kexec,$KERNEL,$INITRD,"$CMDLINE"
